1c8686ab06be669c5ef82cb765ef0e1b64c96410
shellever <shellever@163.com>
2017-10-23 03:44:17 +0800

    more than one ways to list the resulotion of image

--- samples/images/cmd_identify.sh
+++ samples/images/cmd_identify.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 identify $1 | awk '{print $3}' | awk -Fx '{print $1,$2}'
+
+identify 165971.jpg | awk '{print $3}' | awk -Fx '{print $1,$2}'
+
+identify 165971.jpg | awk '{split($3, tA, "x"); print tA[1],tA[2];}'
