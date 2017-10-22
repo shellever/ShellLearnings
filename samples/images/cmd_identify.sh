b299126e1c4be3edc118aacb849b88700a4d6e8e
shellever <shellever@163.com>
2017-10-23 03:55:21 +0800

    delete redundant command line

--- samples/images/cmd_identify.sh
+++ samples/images/cmd_identify.sh
@@ -2,6 +2,4 @@
 
 identify $1 | awk '{print $3}' | awk -Fx '{print $1,$2}'
 
-identify 165971.jpg | awk '{print $3}' | awk -Fx '{print $1,$2}'
-
 identify 165971.jpg | awk '{split($3, tA, "x"); print tA[1],tA[2];}'
