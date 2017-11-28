9a6da36e8837e048f731db47fb68511943dd8fc8
shellever <shellever@163.com>
2017-11-29 01:27:49 +0800

    take an example for expect script

---
+++ expect/login.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "account: " ACCOUNT
+read -p "password: " PASSWORD 
+
+echo "account:\"$ACCOUNT\", password:\"$PASSWORD\""
+echo
---
+++ expect/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ACCOUNT="Shellever"
+PASSWORD="123456"
+
+expect ./expect.exp $ACCOUNT $PASSWORD
+
